57a73bf29fc5b14ecfac4ab7dc7fc047695fd9c8
Aniss Outaleb <aniss1.outaleb@epita.fr>
2021-01-27 00:07:29 +0100

    Parallax and margin fixed

--- content/home.php
+++ content/home.php
@@ -1,7 +1,6 @@
 
-<img src="" id="background">
+<div class="background"></div> 
 <div class="center-box">
-   
     <div id="bigtitle">Beyond Styx</div>
     <div id="description"><?php echo ($txt->{'description'})?></div>
 </div>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="style.css?<? echo time(); ?>" rel="stylesheet">
+<link href="style.css?<?php echo time()?>" rel="stylesheet">
 <title>Projet S2</title>
 </head>
 
