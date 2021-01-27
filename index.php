21f437d47bfd45909ed9e4a288b3f51cfb9d20c4
Aniss Outaleb <aniss1.outaleb@epita.fr>
2021-01-28 00:00:56 +0100

    include

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 
 <body>
 
-<?php //set the language
-
+<?php 
+//set the language
 $lang="fr";
 if( isset( $_GET['lang'] ) ) {
     $lang=$_GET['lang']; 
@@ -17,6 +17,12 @@ if( isset( $_GET['lang'] ) ) {
 
 $json=file_get_contents("languages/".$lang.".json");
 $txt = json_decode($json);
+
+//set include
+$page="home";
+if( isset( $_GET['page'] ) ) {
+  $page=$_GET['page']; 
+}
 ?>
 
 <nav id="nav" style="background-color: #FFE5D0;">
@@ -25,9 +31,9 @@ $txt = json_decode($json);
   <li class="dropdown">
     <a href="javascript:void(0)" class="dropbtn"><?php echo ($txt->{'buttons'})[1]?></a>
     <div class="dropdown-content">
-      <a href="#"><?php echo ($txt->{'dropdown'})[0]?></a>
-      <a href="#"><?php echo ($txt->{'dropdown'})[1]?></a>
-      <a href="index.php?lang=<?php if($lang=="en"){echo "fr";}else{echo "en";}?>"><?php echo ($txt->{'dropdown'})[2]?></a>
+      <a href="index.php?page=home&amp;lang=<?php echo $lang?>" ><?php echo ($txt->{'dropdown'})[0]?></a>
+      <a href="index.php?page=download&amp;lang=<?php echo $lang?>"><?php echo ($txt->{'dropdown'})[1]?></a>
+      <a href="index.php?page=<?php echo $page?>&amp;lang=<?php if($lang=="en"){echo "fr";}else{echo "en";}?>"><?php echo ($txt->{'dropdown'})[2]?></a>
     </div>
   </li>
 </ul>
@@ -36,7 +42,7 @@ $txt = json_decode($json);
 
 <div class="content">
 <?php
-include "content/home.php"
+include "content/".$page.".php"
 ?>
 
 </div>
