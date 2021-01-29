077cc411203099be31feb09f82fd24abbe879be3
Aniss Outaleb <aniss1.outaleb@epita.fr>
2021-01-29 12:13:56 +0100

    .

--- content/team.php
+++ content/team.php
@@ -1,20 +1,24 @@
 <div class="tablecenter">
     <div class="table">
         <div class="member">
-            <span class="tableimg">F</span>
+            <span class="tableimg">Random</span>
             <span class="tabletxt">ddd</span>
+            <span class="workicon"></span>
         </div>
         <div class="member">
-            <span class="tableimg">F</span>
+            <span class="tableimg">Moiise</span>
             <span class="tabletxt">df</span>
+            <span class="workicon"></span>
         </div>
         <div class="member">
-            <span class="tableimg">F</span>
-            <span class="tabletxt">df</span>
+            <span class="tableimg">Thomas</span>
+            <span class="tabletxt">ddddddssssssssssssssssssssssssdddd</span>
+            <span class="workicon">ss</span>
         </div>
         <div class="member">
-            <span class="tableimg">F</span>
+            <span class="tableimg">Pierre</span>
             <span class="tabletxt">ddd</span>
+            <span class="workicon"></span>
         </div>
     </div>
 </div>
