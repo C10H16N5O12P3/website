8a6637043c7e1c4d03015674b4e37c78fc262187
Aniss Outaleb <aniss1.outaleb@epita.fr>
2021-01-29 12:55:24 +0100

    working on display table

--- content/team.php
+++ content/team.php
@@ -1,24 +1,15 @@
-<div class="tablecenter">
-    <div class="table">
-        <div class="member">
-            <span class="tableimg">Random</span>
-            <span class="tabletxt">ddd</span>
-            <span class="workicon"></span>
-        </div>
-        <div class="member">
-            <span class="tableimg">Moiise</span>
-            <span class="tabletxt">df</span>
-            <span class="workicon"></span>
-        </div>
-        <div class="member">
-            <span class="tableimg">Thomas</span>
-            <span class="tabletxt">ddddddssssssssssssssssssssssssdddd</span>
-            <span class="workicon">ss</span>
-        </div>
-        <div class="member">
-            <span class="tableimg">Pierre</span>
-            <span class="tabletxt">ddd</span>
-            <span class="workicon"></span>
-        </div>
+<div class="table">
+    <div class="colonne">
+        <span>sdsd</span>
+        <br><span>sdsd</span>
+    </div>
+    <div class="colonne">
+        sdsd
+    </div>
+    <div class="colonne">
+        sdsdsd
+    </div>
+    <div class="colonne">
+        sdsdsd
     </div>
 </div>
