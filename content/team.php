8c73131791ebe33e9a1cc88d97108ac5740ceab1
Aniss Outaleb <aniss1.outaleb@epita.fr>
2021-02-27 13:07:02 +0100

    change icon disposition

--- content/team.php
+++ content/team.php
@@ -2,7 +2,9 @@
     <div style="margin-top: 63px"></div>
     <div class="content_project">
     <div id="bigtitle" style="text-align: center;"><?php echo ($txt->{"team_title"})?></div>
-    <table id="tbl">
+    <table id="tbl" style="width: 100%;
+  border-spacing: 10px;
+  border-collapse: separate;">
     <tr>
     <tr style="" id="">
         <td><?php echo ($txt->{"team"}[0]->{"name"})?></td>
@@ -10,17 +12,56 @@
         <td><?php echo ($txt->{"team"}[2]->{"name"})?></td>
         <td><?php echo ($txt->{"team"}[3]->{"name"})?></td>
     </tr>
-    <tr style="background-color: #512500; color: white">
-        <td><?php echo ($txt->{"team"}[0]->{"txt"})?></td>
-        <td><?php echo ($txt->{"team"}[1]->{"txt"})?></td>
-        <td><?php echo ($txt->{"team"}[2]->{"txt"})?></td>
-        <td><?php echo ($txt->{"team"}[3]->{"txt"})?></td>
+    <tr style="background-color: #512500; color: white;" id="test">
+        <td class="tbl_txt"><?php echo ($txt->{"team"}[0]->{"txt"})?></td>
+        <td class="tbl_txt"><?php echo ($txt->{"team"}[1]->{"txt"})?></td>
+        <td class="tbl_txt"><?php echo ($txt->{"team"}[2]->{"txt"})?></td>
+        <td class="tbl_txt"><?php echo ($txt->{"team"}[3]->{"txt"})?></td>
     </tr>
     <tr>
-        <td><?php foreach($txt->{"team"}[0]->{"work"} as $src){echo "<span title='".$src."'><img src='icon/work/".$src.".png'></span> ";}?></td>
-        <td><?php foreach($txt->{"team"}[1]->{"work"} as $src){echo "<span title='".$src."'><img src='icon/work/".$src.".png'></span> ";}?></td>
-        <td><?php foreach($txt->{"team"}[2]->{"work"} as $src){echo "<span title='".$src."'><img src='icon/work/".$src.".png'></span> ";}?></td>
-        <td><?php foreach($txt->{"team"}[3]->{"work"} as $src){echo "<span title='".$src."'><img src='icon/work/".$src.".png'></span> ";}?></td>
+        <td>
+            <table id="tbl">
+                <tr>
+                    <?php foreach($txt->{"team"}[0]->{"work"} as $src){echo "<td><span title='".$src."'><img src='icon/work/".$src.".png'></span></td> ";}?>
+                </tr>
+                <tr>
+                <?php foreach($txt->{"team"}[0]->{"work"} as $src){echo "<td>".$src."</td> ";}?>
+                </tr>
+            </table>
+        </td>
+        
+        <td>
+            <table id="tbl">
+                <tr>
+                    <?php foreach($txt->{"team"}[1]->{"work"} as $src){echo "<td><span title='".$src."'><img src='icon/work/".$src.".png'></span></td> ";}?>
+                </tr>
+                <tr>
+                <?php foreach($txt->{"team"}[1]->{"work"} as $src){echo "<td>".$src."</td> ";}?>
+                </tr>
+            </table>
+        </td>
+        
+        <td>
+            <table id="tbl">
+                <tr>
+                    <?php foreach($txt->{"team"}[2]->{"work"} as $src){echo "<td><span title='".$src."'><img src='icon/work/".$src.".png'></span></td> ";}?>
+                </tr>
+                <tr>
+                <?php foreach($txt->{"team"}[2]->{"work"} as $src){echo "<td>".$src."</td> ";}?>
+                </tr>
+            </table>
+        </td>
+        
+        <td>
+            <table id="tbl">
+                <tr>
+                    <?php foreach($txt->{"team"}[3]->{"work"} as $src){echo "<td><span title='".$src."'><img src='icon/work/".$src.".png'></span></td> ";}?>
+                </tr>
+                <tr>
+                <?php foreach($txt->{"team"}[3]->{"work"} as $src){echo "<td>".$src."</td> ";}?>
+                </tr>
+            </table>
+        </td>
         
     </tr>
     </table>
