6f55927826e76fbe07681c2889fe10c5c7d716e0
Aniss Outaleb <aniss1.outaleb@epita.fr>
2021-03-01 15:29:31 +0100

    add link in project

--- content/project.php
+++ content/project.php
@@ -8,13 +8,27 @@
 
         <details>
             <summary><?php echo ($txt->{"doc"}[0]->{"summary"})?></summary>
-            <iframe src="documents/cdc.pdf" width="100%" height="500px">
-                <a href="documents/cdc.pdf"><?php echo ($txt->{"doc"}[0]->{"details"})?></a>
-            </iframe>
+            <div class="soft">
+            <?php 
+                echo ($txt->{"doc"}[0]->{"details"});
+                foreach ($txt->{"doc"}[0]->{"soft"} as $soft){
+                    $soft_name = $soft->{"name"};
+                    $soft_url = $soft -> {"url"};
+                    echo "<br><a href='".$soft_url."' onclick='window.open(this.href); return false'>$soft_name</a>";
+                }
+            ?>
+            </div>
         </details>
 
         <details>
             <summary><?php echo ($txt->{"doc"}[1]->{"summary"})?></summary>
+            <iframe src="documents/cdc.pdf" width="100%" height="500px">
+                <a href="documents/cdc.pdf"><?php echo ($txt->{"doc"}[1]->{"details"})?></a>
+            </iframe>
+        </details>
+
+        <details>
+            <summary><?php echo ($txt->{"doc"}[2]->{"summary"})?></summary>
             <iframe src="documents/cdc.pdf" width="100%" height="500px">
                 <a href="documents/cdc.pdf"><?php echo ($txt->{"doc"}[1]->{"details"})?></a>
             </iframe>
