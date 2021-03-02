096150f589c35fd1a35718bd745db6bc43bbfc22
Aniss Outaleb <aniss1.outaleb@epita.fr>
2021-03-02 19:29:47 +0100

    add 1st presentation, add progress title

--- content/project.php
+++ content/project.php
@@ -1,6 +1,7 @@
 <span class="globe">
 <div style="margin-top: 63px"></div>
-<div class="content_project">    
+<div class="content_project">
+    <!-- Documentation -->
     <div id="bigtitle" style="text-align: center;"><?php echo ($txt->{"doc_title"})?></div>
         <div style="text-align: center; ">
             <?php echo ($txt->{"doc_description"})?>
@@ -29,12 +30,30 @@
 
         <details>
             <summary><?php echo ($txt->{"doc"}[2]->{"summary"})?></summary>
-            <iframe src="documents/cdc.pdf" width="100%" height="500px">
-                <a href="documents/cdc.pdf"><?php echo ($txt->{"doc"}[1]->{"details"})?></a>
+            <iframe src="documents/1st.pdf" width="100%" height="500px">
+                <a href="documents/1st.pdf"><?php echo ($txt->{"doc"}[2]->{"details"})?></a>
             </iframe>
         </details>
 
+        <details>
+            <summary><?php echo ($txt->{"doc"}[3]->{"summary"})?></summary>
+            <iframe src="documents/cdc.pdf" width="100%" height="500px">
+                <a href="documents/cdc.pdf"><?php echo ($txt->{"doc"}[3]->{"details"})?></a>
+            </iframe>
+        </details>
 
+        <details>
+            <summary><?php echo ($txt->{"doc"}[4]->{"summary"})?></summary>
+            <iframe src="documents/cdc.pdf" width="100%" height="500px">
+                <a href="documents/cdc.pdf"><?php echo ($txt->{"doc"}[4]->{"details"})?></a>
+            </iframe>
+        </details>
+
+    <!-- Progression -->
+    <div id="bigtitle" style="text-align: center;"><?php echo ($txt->{"progress_title"})?></div>
+        <div style="text-align: center; ">
+            <?php echo ($txt->{"progress_description"})?>
+        </div> 
 </div>
 
     
