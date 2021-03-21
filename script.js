ae7c1818e8af22f95e2823f7c8d4b9b092a928e3
Aniss Outaleb <aniss1.outaleb@epita.fr>
2021-03-21 16:49:39 +0100

    position fixed body

--- script.js
+++ script.js
@@ -7,41 +7,14 @@ function menu(){
     if(document.getElementById("nav").style.display == "block"){
         document.getElementById("nav").style.display = "none";
         document.getElementById("respmenu").style.display = "none";
-        document.getElementById("respbar").style.display = "block";
+        document.body.style.position = "unset";
+        
     }
     else{
         document.getElementById("nav").style.display = "block";
         document.getElementById("respmenu").style.display = "block";
-        document.getElementById("respbar").style.display = "none";
+        document.body.style.position = "fixed";
     }
 }
 
 //home menu
-function button_color(buttons, color){
-    for(var i=1; i<buttons.length; i++) {
-        buttons[i].style.color= color;
-    }
-}
-
-nav = document.getElementById("nav"); 
-buttons = Array.prototype.slice.call(document.getElementsByTagName("a"));
-
-function home(){
-    if(document.getElementById("nav").style.display == "block"){
-        nav.style.backgroundColor="transparent";
-        button_color(buttons, "white");
-        window.addEventListener('scroll', () => {
-        let y = window.scrollY ; 
-        if(y<500){ //scrolling down
-            nav.style.backgroundColor="transparent"; 
-            button_color(buttons, "white"); 
-        }
-        else{ //scrolling up
-            nav.style.backgroundColor="#FFE5D0";
-            button_color(buttons, "black");
-        }
-        }
-        )
-    }
-    
-}
