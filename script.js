af59a8c891ae7bf75c31bffbebfdfc36b66d7f59
Aniss Outaleb <aniss1.outaleb@epita.fr>
2021-04-02 09:39:31 +0200

    overflow : hidden

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function menu(){
     if(document.getElementById("nav").style.display == "block"){
         document.getElementById("nav").style.display = "none";
         document.getElementById("respmenu").style.display = "none";
-        document.body.style.position = "unset";
+        document.body.style.overflow = "scroll";
         
     }
     else{
         document.getElementById("nav").style.display = "block";
         document.getElementById("respmenu").style.display = "block";
-        document.body.style.position = "fixed";
+        document.body.style.overflow = "hidden";
     }
 }
 
