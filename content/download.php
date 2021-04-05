69f255c9a5c5ccf1bcea999834ff1ceaffe1649e
Aniss Outaleb <aniss1.outaleb@epita.fr>
2021-04-06 00:04:09 +0200

    add table

--- content/download.php
+++ content/download.php
@@ -1,44 +1,42 @@
-<<<<<<< HEAD
-<div class="down_content">
+<span class="globe">
+<div style="margin-top: 63px"></div>
+<div class="download_content">
 <div id="bigtitle" style="text-align:center">Télécharger</div>
-<table class="down_table" style="width:100%; background: #512500; color: white" >
-    <tr style="text-align:center">
-        <td></td>
-        <td colspan="2">Version</td>
-    </tr>
-    <tr>
-        <td>OS</td>
-        <td style="text-align:center">1.0</td>
-        <td style="text-align:center">Alpha</td>
-    </tr>
-    <tr>
-        <td colspan="3" style="background: #351800; color: white">Windows</td>
-    </tr>
-    <tr>
-        <td>Installeur</td>
-        <td style="text-align:center"><a href="">click</a></td>
-        <td style="text-align:center"><a href="">click</a></td>
-    </tr>
-    <tr>
-        <td>Archive ZIP</td>
-        <td style="text-align:center"><a href="">click</a></td>
-        <td style="text-align:center"><a href="">click</a></td>
-    </tr>
-</table>
+<div class="pc">
+    <table class="down_table" style="width:100%; background: #512500; color: white" >
+        <tr style="text-align:center">
+            <td></td>
+            <td colspan="2">Version</td>
+        </tr>
+        <tr>
+            <td>OS</td>
+            <td style="text-align:center">1.0</td>
+            <td style="text-align:center">Alpha</td>
+        </tr>
+        <tr>
+            <td colspan="3" style="background: #351800; color: white">Windows</td>
+        </tr>
+        <tr>
+            <td>Installeur</td>
+            <td style="text-align:center"><a href="">click</a></td>
+            <td style="text-align:center"><a href="">click</a></td>
+        </tr>
+        <tr>
+            <td>Archive ZIP</td>
+            <td style="text-align:center"><a href="">click</a></td>
+            <td style="text-align:center"><a href="">click</a></td>
+        </tr>
+    </table>
+</div>
+
+<div class="responsive">
+    
+</div>
 
 <div id="bigtitle" style="text-align:center">Comment installer</div>
 <div id="bigtitle" style="text-align:center">Comment jouer</div>
 
 </div> 
-=======
-
-
-<span class=globe>
-<div class="center-center"> 
-<?php echo ($txt->{"download"})?>
-<div id="bigtitle">07/06/21</div>
-
-</div>
 
 <div class="content_project">
 </div>
@@ -46,4 +44,3 @@
     <?php include "content/footer.php"?>
 </footer>
 </span>
->>>>>>> main
