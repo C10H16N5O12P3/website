51f46da4aa25f78def02cff9b34a07006701f671
Aniss Outaleb <aniss1.outaleb@epita.fr>
2021-04-26 03:19:07 +0200

    json card

--- content/download.php
+++ content/download.php
@@ -4,8 +4,8 @@
 <div id="bigtitle" style="text-align:center">Télécharger</div>
 <div class="">
     <div class ="card_box">
-        <div class="card"><div class="card_background"><img src="icon/download/windows.png"></div><div class="card_description">Version pour windows<br>Ceci est une demo</br></div></div>
-        <a href=""><div class="card"><div class="card_background"><img src="icon/download/linux.png"></div><div class="card_description">Version pour linux<br>Ceci est une demo</br></div></div></a>
+        <div class="card"><div class="card_background"><img src="icon/download/windows.png"></div><div class="card_description"><?php echo ($txt->{'card'})[0]->{'description'}?><br><?php echo ($txt->{'card'})[0]->{'txt'}?></br></div></div>
+        <a href=""><div class="card"><div class="card_background"><img src="icon/download/linux.png"></div><div class="card_description"><?php echo ($txt->{'card'})[1]->{'description'}?><br><?php echo ($txt->{'card'})[1]->{'txt'}?></br></div></div></a>
     </div>
 </div>
 <div class="responsive">
