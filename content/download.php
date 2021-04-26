a9debde31004c7faec6b1fc140e22fdb460df209
Aniss Outaleb <aniss1.outaleb@epita.fr>
2021-04-26 03:01:38 +0200

    add card

--- content/download.php
+++ content/download.php
@@ -1,11 +1,19 @@
-
-
-<span class=globe>
-<div class="center-center"> 
-<?php echo ($txt->{"download"})?>
-<div id="bigtitle">07/06/21</div>
-
+<span class="globe">
+<div style="margin-top: 63px"></div>
+<div class="download_content">
+<div id="bigtitle" style="text-align:center">Télécharger</div>
+<div class="">
+    <div class ="card_box">
+        <div class="card"><div class="card_background"><img src="icon/download/windows.png" width="125px"></div><div class="card_description">Version pour windows<br>Ceci est une demo</br></div></div>
+        <a href=""><div class="card"><div class="card_background"><img src="icon/download/linux.png" width="125px"></div><div class="card_description">Version pour linux<br>Ceci est une demo</br></div></div></a>
+    </div>
 </div>
+<div class="responsive">
+    
+</div>
+
+
+</div> 
 
 <div class="content_project">
 </div>
